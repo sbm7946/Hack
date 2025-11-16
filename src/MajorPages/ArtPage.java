82b07ab699793c40642e64b1d75c85426591f73a
Siena <sbullockmartin7946@sdsu.edu>
2025-11-16 14:19:03 -0800

    Making Major Page The Same

--- src/MajorPages/ArtPage.java
+++ src/MajorPages/ArtPage.java
@@ -12,7 +12,16 @@ public class ArtPage extends MajorsPageADT{
 
 
     public ArtPage() {
-        super("Art");
+        super("Postings ");
+        setLayout(new BorderLayout());
+        JLabel title = new JLabel("Postings!", SwingConstants.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 24));
+        JTextArea info = new JTextArea("empty :/ ");
+        info.setEditable(false);
+        info.setFont(new Font("Monospaced", Font.PLAIN, 14));
+        info.setBackground(getBackground());
+        add(title, BorderLayout.NORTH);
+        add(new JScrollPane(info), BorderLayout.CENTER);
 
         
 
